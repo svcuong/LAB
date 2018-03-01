430f17104c89606a75ff924fce92373cadce86f0
Cuong.S <31816408+svcuonghvktqs@users.noreply.github.com>
2018-03-01 06:44:32 -0800

    added task_lab3

---
+++ LAB 3/code_lab3/task_lab3.R
@@ -0,0 +1,41 @@
+
+#___________Задания для Лабораторной работы № 3__________________________
+
+##########################################################################
+
+# Задание 1: загрузка набора данных AirPassengers и необходимых пакетов для, 
+# деления набора данных на train data и test data
+# прогнозирования
+library(ggplot2)
+library(forecast)
+
+x <- AirPassengers
+
+length(x)
+k <- time(x)[100]
+# деления набора данных на train data и test data
+
+trainx <- window(x, end=k-1)
+testx <- window(x, start= k)
+#########################################################################
+
+# Задание 2: Использование метода Benchmark forecasting - naive для прогнозирования
+
+fc <- snaive(trainx)
+fc
+autoplot(fc)
+
+# Задание 3: Использование метода Использование Exponential smoothing для прогнозирования
+
+fitets <- ets(trainx)
+fcfitets
+autoplot(fcfitets)
+
+# Задание 4: Использование метода Использование Exponential smoothing для прогнозирования
+arimats <- auto.arima(trainx)
+fcarimats <- forecast(arimats)
+autoplot(fcarimats)
+
+#______________________LAB 3_________________________________________________________
+
+
