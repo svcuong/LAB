5d452ff90f4a23ae4c44f81b98f1e1336c579194
Cuong.S <31816408+svcuonghvktqs@users.noreply.github.com>
2018-03-01 04:50:13 -0800

    added task_lab2

--- LAB 2/code_lab2/task_lab2.R
+++ LAB 2/code_lab2/task_lab2.R
@@ -1,8 +1,8 @@
 
-#___________________ Задания для Лабораторной работы № 1___________________________________________
+#___________________ Задания для Лабораторной работы № 2___________________________________________
 
 ############################################################
-# Задание 1: Загрузка набора данных из интернета и присвоение имение mydat
+# Задание 1: Загрузка набора данных (Labour Training Evaluation Data) из источника данных:
 #url<https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/DAAG/nsw74psid1.csv>
 
 library(RCurl)
@@ -10,80 +10,60 @@ myfile <- getURL('https://raw.githubusercontent.com/vincentarelbundock/Rdatasets
 mydat <- read.csv(textConnection(myfile), header=T)
 head(mydat)
 
-#############################################################
-# Задание 2: Получение свойства загруженного набора данных
+# Задание 1.1: Построение scatter plot для columns age и re74 из mydat с использованием базовой функции
+plot(mydat$age, mydat$re74)
 
-# посмотреть начальные строки объекта
+# Задание 1.2: Построение scatter plot для columns age и re74 из mydat с использованием пакета ggplot2
+library(ggplot2)
+qplot(mydat$age, mydat$re74)
+
+# или:
+qplot(age, re74, data=mydat)
+
+# или:
+ggplot(mydat, aes(x=age, y=re74)) + geom_point()
+
+# Задание 1.3: Построение Box Plot для re74 от black из mydat с использованием базовой функции plot()
+plot(as.factor(mydat$black), mydat$re74)
+# или:
+boxplot(re74 ~ black, data = mydat)
+
+# Задание 1.4: Построение Box Plot для re74 от black из mydat с использованием пакета ggplot2
+library(ggplot2)
+qplot(as.factor(mydat$black), mydat$re74, geom="boxplot")
+# или:
+ggplot(mydat, aes(x=as.factor(black), y=re74)) + geom_boxplot()
+
+# Задание 1.5: Вычисление количество элементов с black =0 и black =0. Создание фрейма данных из релуьтатов 
+# вычисления с 2 columns black и count, и построение bar graph для этого фрейма.
+black <- as.factor(c(0, 1))
+count0 <- length(subset(mydat, black == 0)$black)
+count1 <- length(subset(mydat, black == 1)$black)
+count <- c(count0, count1)
+df <- data.frame(black, count)
+df
+barplot(df$count, names.arg=df$black)
+
+####################################################################
+
+# Задание 2: Загрузка набора данных AirPassengers из источника данных: 
+# url <https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/AirPassengers.csv>
+myfile <- getURL('https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/AirPassengers.csv', ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+mydat <- read.csv(textConnection(myfile), header=T)
 head(mydat)
 
-# посмотреть последние строки объекта
-tail(mydat)
+#Задание 2.1: построение Line Graph зависимости AirPassengers от time для этого набора данных с использованием
+# базовой функции.
+plot(mydat$time, mydat$AirPassengers, type = "l")
 
-# общая информация об объекте
-summary(mydat)
-
-# внутренняя структура объекта
-str(mydat)
-
-# размерности объекта
-dim(mydat)
-
-# имена размерностей объекта
-dimnames(mydat)
-
-# имена объекта
-names(mydat)
-
-# число элементов
-length(mydat)
-##################################################################
-
-
-
-# Задание 3: Создание нового набора данных с именем newdat из 100-первых строк исходного набора данных mydat
-newdat <- mydat[1:100,]
-View(newdat)
-
-####################################################################
-
-## Задание 4: Создание нового набора данных с именем newdat1 newdat с условием age >= 40 И вычисление количество элементов с таким условием
-
-newdat1 <- subset(newdat, age >= 40)
-View(newdat1)
-length(newdat1$age)
+#Задание 2.2:построение Line Graph зависимости AirPassengers от time для этого набора данных с использованием
+# пакета ggpot2
+library(ggplot2)
+qplot(mydat$time, mydat$AirPassengers, geom="line")
+# или:
+qplot(time, AirPassengers, data=mydat, geom="line")
+# или:
+ggplot(mydat, aes(x=time, y=AirPassengers)) + geom_line()
 
 #####################################################################
-
-# Задание 5: Сортировка набора данных newdat1 по age
-newdat1[order(newdat1$age),]
-
-#####################################################################
-
-# Задание 6: вычисление количества элементов исходного набора данных mydat с условием black = 0
-length(subset(mydat, black == 0)$black)
-
-####################################################################
-
-# Задание 7: вычисление количества элементов исходного набора данных mydat с условием black = 0 и age >= 50
-length(subset(mydat, black == 0 & age >= 50)$black)
-
-####################################################################
-
-# Задание 8: Создание нового набора данных с имемем newdat2 из исходного набора данных mydat с условием  marr=1 & educ=12
-
-newdat2 <- subset(mydat, marr == 1 & educ ==12)
-View(newdat2)
-
-####################################################################
-
-# Задание 9: Сортирока набора данных newdat2 по educ
-newdat2[order(newdat2$educ),]
-
-####################################################################
-# Задание 10: Удаление элементов с re74=0 из newdat2
-
-newdat2 <- subset(newdat2, re74 != 0 )
-View(newdat2)
-
-#___________________ Задания для Лабораторной работы № 1___________________________________________
 
