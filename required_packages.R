d5d515a8a6ea62cf933ecfa32ef7f47ed479bdd5
Cuong.S <31816408+svcuonghvktqs@users.noreply.github.com>
2018-03-02 01:12:43 -0800

    added required_pacakges

---
+++ required_packages.R
@@ -0,0 +1,17 @@
+
+# Install all of required packages for LABs
+
+
+install.packages("readr") # friendly way to read rectangular data (like 'csv', 'tsv', and 'fwf')
+install.packages("readxl") # Import excel files into R
+install.packages("Mcomp") # M-competions datasets
+install.packages("RCurl") # to import  files from internet
+install.packages("ggplot2") # for data visualization
+install.packages("forecast") # for time series forecasting
+install.packages("caret") # for training and plotting classificaton and regression model
+install.packages("tidyr") # for data tydying
+install.packages("neuralnet") # For training of neural networks
+
+
+
+
